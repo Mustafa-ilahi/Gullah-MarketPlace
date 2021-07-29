84e2e5049b93b8237894bc41d56f4b8468e92869
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-07-30 01:25:54 +0500

    updated

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { login } from "../../config/firebase"
-
+import './index.css'
 function Login(){
     const [email,setEmail] = useState()
     const [password,setPassword] = useState()
@@ -15,15 +15,21 @@ function Login(){
         }
     }
     return(
-        <div>
-            <h1>LogIn Form</h1>
-            <label>E-mail* </label>
-            <input placeholder="example@gmail.com" onChange={(e)=>setEmail(e.target.value)}/>
-            <br />
-            <label>Password* </label>
-            <input placeholder="******" onChange={(e)=>setPassword(e.target.value)}/>
-            <br />
-            <button onClick={signIn}>Login</button>
+        <div className="main-div">
+            <div>
+                <h1>Hello world</h1>
+            </div>
+            <div className="login-form">
+
+                <h1 id="heading">Log in to Gullah Marketplace</h1>
+                <label>E-mail* </label>
+                <input placeholder="example@gmail.com" onChange={(e)=>setEmail(e.target.value)}/>
+                <br />
+                <label>Password* </label>
+                <input placeholder="******" onChange={(e)=>setPassword(e.target.value)}/>
+                <br />
+                <button onClick={signIn}>Login</button>
+            </div>
         </div>
     )
 }
