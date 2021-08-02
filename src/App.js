f71cc2d791d365cbe0150a001360be3a8ccfd247
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-08-03 03:07:27 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import Router from './config/router'
+import Dashboard from './views/Dashboard';
 function App() {
   return (
     <div className="App">
